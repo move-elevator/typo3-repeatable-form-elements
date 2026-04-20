43f9a2e7b95d8a96ce53c86625d2ea8139b784fe
Konrad Michalik <km@move-elevator.de>
2026-04-20 14:28:15 +0200

    feat: update version constraints for TYPO3 v13/v14 support
    Drop TYPO3 v12 support. Target v13.4 LTS and v14.x.
    Bump extension version to 6.0.0.

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF['repeatable_form_elements'] = [
     'state' => 'stable',
     'author' => 'Ralf Zimmermann, Elias Häußler, Christian Seyfferth',
     'author_email' => 'r.zimmermann@dreistrom.land, elias@haeussler.dev, c.seyfferth@dreistrom.land',
-    'version' => '5.0.0',
+    'version' => '6.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '13.4.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
