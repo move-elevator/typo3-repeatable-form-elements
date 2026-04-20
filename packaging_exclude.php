38abfa403bbffcb4439c44a96204c4a6fa624570
Konrad Michalik <km@move-elevator.de>
2026-04-20 16:37:26 +0200

    chore: add EditorConfig, packaging excludes and version bumper config

---
+++ packaging_exclude.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'directories' => [
+        '.Build',
+        '.ddev',
+        '.git',
+        '.github',
+        'Documentation',
+        'docs',
+        'public',
+        'Tests',
+        'var',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'CODE_OF_CONDUCT.md',
+        'CODEOWNERS',
+        'composer.lock',
+        'CONTRIBUTING.md',
+        'editorconfig',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+        'phpunit.xml',
+        'renovate.json',
+        'version-bumper.yaml',
+    ],
+];
